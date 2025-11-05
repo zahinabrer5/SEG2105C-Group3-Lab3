5ba36607423c1b41251ba96535fa474c4951f014
Abdullah <66182508+zahinabrer5@users.noreply.github.com>
2025-11-04 21:32:07 -0500

    Initial commit (mostly finished)

---
+++ app/src/main/java/org/abdullah/lab3databases/MainActivity.java
@@ -0,0 +1,118 @@
+package org.abdullah.lab3databases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class MainActivity extends AppCompatActivity {
+    TextView productId;
+    EditText productName, productPrice;
+    Button addBtn, findBtn, deleteBtn;
+    ListView productListView;
+
+    ArrayList<String> productList;
+    ArrayAdapter<String> adapter;
+    MyDBHandler dbHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        productList = new ArrayList<>();
+
+        // info layout
+        productId = findViewById(R.id.productId);
+        productName = findViewById(R.id.productName);
+        productPrice = findViewById(R.id.productPrice);
+
+        //buttons
+        addBtn = findViewById(R.id.addBtn);
+        findBtn = findViewById(R.id.findBtn);
+        deleteBtn = findViewById(R.id.deleteBtn);
+
+        // listview
+        productListView = findViewById(R.id.productListView);
+
+        // db handler
+        dbHandler = new MyDBHandler(this);
+
+        // button listeners
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = productName.getText().toString();
+                double price = Double.parseDouble(productPrice.getText().toString());
+                Product product = new Product(name, price);
+                dbHandler.addProduct(product);
+
+                productName.setText("");
+                productPrice.setText("");
+
+//                Toast.makeText(MainActivity.this, "Add product", Toast.LENGTH_SHORT).show();
+                viewProducts();
+            }
+        });
+
+        findBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = productName.getText().toString();
+                Product product = dbHandler.findProduct(name);
+
+                if (product != null) {
+                    String price = String.valueOf((int)product.getProductPrice());
+                    productPrice.setText(price);
+                }
+
+                Toast.makeText(MainActivity.this, "Attempted to find product and filled in the price field for you!", Toast.LENGTH_SHORT).show();
+                viewProducts();
+            }
+        });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = productName.getText().toString();
+
+//                String message = "Deleted product named "+name;
+//                if (!dbHandler.deleteProduct(name))
+//                    message = "Unable to find product named"+name;
+                dbHandler.deleteProduct(name);
+                String message = "Attempted to delete product named "+name;
+
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                viewProducts();
+            }
+        });
+
+
+        viewProducts();
+    }
+
+    private void viewProducts() {
+        productList.clear();
+        Cursor cursor = dbHandler.getData();
+        if (cursor.getCount() == 0) {
+            Toast.makeText(MainActivity.this, "Nothing to show", Toast.LENGTH_SHORT).show();
+        } else {
+            while (cursor.moveToNext()) {
+                productList.add(cursor.getString(1) + " (" +cursor.getString(2)+")");
+            }
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, productList);
+        productListView.setAdapter(adapter);
+    }
+}
